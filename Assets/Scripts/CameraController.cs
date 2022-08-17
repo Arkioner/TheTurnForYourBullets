5011783c97f7972fbc5dd03a677cba522d088e3c
Arkioner <ioner468@gmail.com>
2022-08-17 23:56:11 +0200

    clean up and add zoom to camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private CinemachineVirtualCamera cmvCamera;
+    [SerializeField] private int minZoom = 1;
+    [SerializeField] private int maxZoom = 10;
+    private CinemachineTransposer cinemachineTransposer;
+
     private void Start()
     {
+        cinemachineTransposer = cmvCamera.GetCinemachineComponent<CinemachineTransposer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Vector3 inputMoveDir = Vector3.zero;
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+
+    private void HandleZoom()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            cinemachineTransposer.m_FollowOffset.y = Math.Clamp(
+                cinemachineTransposer.m_FollowOffset.y + Input.mouseScrollDelta.y,
+                minZoom,
+                maxZoom
+            );
+        }
+    }
+
+    private void HandleRotation()
+    {
         Vector3 rotation = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.Q))
+            rotation.y = 90f;
+        if (Input.GetKeyDown(KeyCode.E))
+            rotation.y = -90;
+        transform.eulerAngles += rotation;
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 inputMoveDir = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
             inputMoveDir.z = 1f;
         if (Input.GetKey(KeyCode.A))
@@ -22,12 +55,6 @@ public class CameraController : MonoBehaviour
             inputMoveDir.z = -1f;
         if (Input.GetKey(KeyCode.D))
             inputMoveDir.x = 1f;
-        if (Input.GetKeyDown(KeyCode.Q))
-            rotation.y = 90f;
-        if (Input.GetKeyDown(KeyCode.E))
-            rotation.y = -90;
-        
         transform.position += inputMoveDir.z * transform.forward + inputMoveDir.x * transform.right;
-        transform.eulerAngles += rotation;
     }
 }
